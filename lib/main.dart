5c33387221a3eb601bc309692521019b7acbef60
dhruvcodex <dhruvtandon30@gmail.com>
2024-09-28 00:28:48 +0530

    chat application

---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+
+
+
+//Services
+import './services/navigation_service.dart';
+
+//Providers
+import './providers/authentication_provider.dart';
+
+//Pages
+import './pages/splash_page.dart';
+import './pages/login_page.dart';
+import './pages/register_page.dart';
+import './pages/home_page.dart';
+import './firebase_options.dart';
+
+void main() async{
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
+  runApp(
+    SplashPage(
+      key: UniqueKey(),
+      onInitializationComplete: () {
+        runApp(
+          MainApp(),
+        );
+      },
+    ),
+  );
+}
+
+class MainApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<AuthenticationProvider>(
+          create: (BuildContext _context) {
+            return AuthenticationProvider();
+          },
+        )
+      ],
+      child: MaterialApp(
+        title: 'Chatify',
+        theme: ThemeData(
+          backgroundColor: Color.fromRGBO(36, 35, 49, 1.0),
+          scaffoldBackgroundColor: Color.fromRGBO(36, 35, 49, 1.0),
+          bottomNavigationBarTheme: BottomNavigationBarThemeData(
+            backgroundColor: Color.fromRGBO(30, 29, 37, 1.0),
+          ),
+        ),
+        navigatorKey: NavigationService.navigatorKey,
+        initialRoute: '/login',
+        routes: {
+          '/login': (BuildContext _context) => LoginPage(),
+          '/register': (BuildContext _context) => RegisterPage(),
+          '/home': (BuildContext _context) => HomePage(),
+        },
+      ),
+    );
+  }
+}
